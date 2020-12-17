5e6d12e53e6a33ceb0c6407be8320716d7170a04
char502 <char502@hotmail.com>
2020-12-17 15:07:38 +0000

    input value working

---
+++ js/app.js
@@ -0,0 +1,29 @@
+
+
+// window.onload = function () {
+//   document.getElementById("donation--button").addEventListener("click", displayValue);
+
+  
+
+// };
+
+
+
+// alert(amountValue)
+
+// let donationButton = document.getElementById("donation--button")
+// donationButton.addEventListener("click", displayValue);
+
+
+// function displayValue(e) {
+//     e.preventDefault()
+//     console.log(amountValue)
+//     alert(amountValue)
+// }
+
+let getInputValue = () => {
+
+    let amountValue = document.getElementById("donation--amount").value
+    alert(amountValue)
+
+}
