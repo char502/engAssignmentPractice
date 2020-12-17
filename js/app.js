65775a842434a0f15298b1948ac3908593e54441
char502 <char502@hotmail.com>
2020-12-17 22:57:06 +0000

    trying to get the logic working

--- js/app.js
+++ js/app.js
@@ -2,9 +2,6 @@
 
 // window.onload = function () {
 //   document.getElementById("donation--button").addEventListener("click", displayValue);
-
-  
-
 // };
 
 
@@ -21,9 +18,83 @@
 //     alert(amountValue)
 // }
 
+let amountValue = 0;
+
+
+
+
 let getInputValue = () => {
 
-    let amountValue = document.getElementById("donation--amount").value
-    alert(amountValue)
+    amountValue = document.getElementById("donation--amount").value
 
+    let splitType = document.getElementById("donation--type")
+    let selectedSplitType = splitType.value
+
+    alert(`You selected: ${amountValue} and ${selectedSplitType}`)
+
+    let newArr = []
+
+            if (selectedSplitType === 'equal') {
+                // let newArr = [];
+                let i = 10;
+        
+                if (amountValue % 10 === 0) {
+                while (amountValue > 0 && i > 0) { 
+                let a = Math.floor(amountValue / i / 10 ) * 10 ; 
+                amountValue -= a; 
+                // console.log(amountValue)
+                i--; 
+                newArr.push(a);
+            } 
+        } else {
+            console.log('not divisible by 10')
+        }
+
+        // newArr.push(amount)
+        // console.log(newArr)
+        
+
+        diaplayResults(newArr)
+        
+        // return newArr
+
+    } else if (selectedSplitType === 'more-odd') {
+
+    }
+
+}
+
+
+const diaplayResults = (newArr) => {
+    console.log(newArr)
+    let inputAmount = document.getElementById("donation--amount").value
+    console.log(inputAmount)
+
+    let splitType = document.getElementById("donation--type")
+    let selectedSplitType = splitType.value
+    console.log(selectedSplitType)
+
+    let firstRow = document.getElementById("donation")
+    let secondRow = document.getElementById("split-type")
+
+    firstRow.innerHTML = `<tr><td>${inputAmount}</td>`
+    secondRow.innerHTML = `<td>${selectedSplitType}</td>`
+
+
+    let tableRows = document.getElementById("dataGoesHere")
+
+
+    for (let i = 0; i < newArr.length; i++) {
+        let row = `<tr>
+                        <td>${newArr[i]}</td>
+                   <tr>`
+        tableRows.innerHTML += row
+
+    }
+}
+
+
+
+let reset = () => {
+    alert('reset button clicked')
 }
