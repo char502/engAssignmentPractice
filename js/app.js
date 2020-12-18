e968af578b55d2860902917ce1afdae6f15e7c04
char502 <char502@hotmail.com>
2020-12-18 08:42:37 +0000

    one test case working all the way through

--- js/app.js
+++ js/app.js
@@ -1,26 +1,4 @@
-
-
-// window.onload = function () {
-//   document.getElementById("donation--button").addEventListener("click", displayValue);
-// };
-
-
-
-// alert(amountValue)
-
-// let donationButton = document.getElementById("donation--button")
-// donationButton.addEventListener("click", displayValue);
-
-
-// function displayValue(e) {
-//     e.preventDefault()
-//     console.log(amountValue)
-//     alert(amountValue)
-// }
-
 let amountValue = 0;
-
-
 
 
 let getInputValue = () => {
@@ -29,13 +7,12 @@ let getInputValue = () => {
 
     let splitType = document.getElementById("donation--type")
     let selectedSplitType = splitType.value
-
-    alert(`You selected: ${amountValue} and ${selectedSplitType}`)
+    // alert(`You selected: ${amountValue} and ${selectedSplitType}`)
 
     let newArr = []
 
             if (selectedSplitType === 'equal') {
-                // let newArr = [];
+                
                 let i = 10;
         
                 if (amountValue % 10 === 0) {
@@ -50,13 +27,7 @@ let getInputValue = () => {
             console.log('not divisible by 10')
         }
 
-        // newArr.push(amount)
-        // console.log(newArr)
-        
-
         diaplayResults(newArr)
-        
-        // return newArr
 
     } else if (selectedSplitType === 'more-odd') {
 
@@ -64,35 +35,36 @@ let getInputValue = () => {
 
 }
 
-
 const diaplayResults = (newArr) => {
-    console.log(newArr)
+    // console.log(newArr)
     let inputAmount = document.getElementById("donation--amount").value
-    console.log(inputAmount)
+    // console.log(inputAmount)
 
     let splitType = document.getElementById("donation--type")
     let selectedSplitType = splitType.value
-    console.log(selectedSplitType)
-
-    let firstRow = document.getElementById("donation")
-    let secondRow = document.getElementById("split-type")
-
-    firstRow.innerHTML = `<tr><td>${inputAmount}</td>`
-    secondRow.innerHTML = `<td>${selectedSplitType}</td>`
+    // console.log(selectedSplitType)
 
 
     let tableRows = document.getElementById("dataGoesHere")
 
+    let row = tableRows.insertRow(0);
+
+    let cell1 = row.insertCell(0)
+    let cell2 = row.insertCell(1)
+
+    cell1.innerHTML = `${inputAmount}`
+    cell2.innerHTML = `${selectedSplitType}`
 
     for (let i = 0; i < newArr.length; i++) {
-        let row = `<tr>
-                        <td>${newArr[i]}</td>
-                   <tr>`
-        tableRows.innerHTML += row
 
+        let counter = 3
+
+        let cell = `cell${counter + i}`
+        cell = row.insertCell(`${counter -1 + i}`)
+
+        cell.innerHTML = `${newArr[i]}`
     }
 }
-
 
 
 let reset = () => {
